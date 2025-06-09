05b7cb46539a41932890a1f3f6bde1ca4a35322d
wimbarelds <wim.barelds@gmail.com>
2025-06-09 22:01:21 +0200

    Add github pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ command }) => {
+  if (command !== 'build') {
+    return {
+      plugins: [react(), tailwindcss()],
+    };
+  }
+  return {
+    base: '/pega-react-training/',
+    plugins: [react(), tailwindcss()],
+  };
 });
